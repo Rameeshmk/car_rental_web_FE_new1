bec22e316908e0e3a515e331cf897ada2457bf65
Rameeshmk <rameeshmk@gmail.com>
2024-09-01 05:57:07 +0300

    Update SignOutButton.jsx

--- src/components/SignOutButton/SignOutButton.jsx
+++ src/components/SignOutButton/SignOutButton.jsx
@@ -22,12 +22,11 @@ const handleSignOut = async () => {
       });
 
       
-      localStorage.removeItem('token');
+      sessionStorage.removeItem('token');
       localStorage.removeItem('userId');
 
       
-      document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-      document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+     
   
 
       navigate('/');
