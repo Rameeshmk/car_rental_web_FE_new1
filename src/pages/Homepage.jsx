0a0f262d7d09b9fdb29351e92a01098ecc6a1449
Rameeshmk <rameeshmk@gmail.com>
2024-09-02 11:38:18 +0300

    remove filter

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,121 +1,10 @@
-import React, { useState } from 'react';
-import DatePickers from '../components/datepicker/DatePickers';
-import UsersCars from '../../src/components/car/UsersCars';
-import FindNow from '../pages/FindNow';
-import UserReviewForm from '../components/userreview/UserReviewForm';
-import CategoryButtons from '../components/category/CotegoryButtons';
-
-const Homepage = () => {
-  const [selectedModel, setSelectedModel] = useState('All');
-
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
-  };
-
-  return (
-    <div>
-      <div className="relative overflow-hidden">
-        {/* Background Image */}
-        <div className="absolute inset-0 -z-10">
-          <img
-            src="https://img.lovepik.com/photo/48007/1912.jpg_wh860.jpg"
-            alt="Background"
-            className="w-full h-full object-cover"
-          />
-        </div>
-
-        {/* Welcome Message */}
-        <section className="relative flex items-center justify-center h-screen text-center bg-gray-800 bg-opacity-60 text-white">
-          <div className="text-center px-4 sm:px-6 lg:px-8">
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4">Welcome to Our Car Rental Service</h1>
-            <p className="text-lg sm:text-xl mb-6">Find the perfect car for your next adventure.</p>
-            <h2 className="text-3xl font-bold mb-8">Ready to Rent Your Dream Car?</h2>
-
-            <div>
-              <FindNow />
-            </div>
-            <div className='mt-8 mb-3'>
-              <UserReviewForm />
-            </div>
-          </div>
-        </section>
-      </div>
-
-      <div>
-        <h1 className="flex my-14 text-white items-center justify-center bg-black text-4xl font-bold text-center mb-8">BOOK YOUR VEHICLES</h1>
-      </div>
-      <div>
-        <h1 className="flex my-32 items-center justify-center bg-white text-2xl font-thin text-center mb-8">RELIABLE VEHICLE HIRE, AS LOW AS PRICE $23/DAY</h1>
-      </div>
-      <div>
-        <h1 className="flex items-center justify-center bg-white text-6xl font-bold text-center mb-8">NEWEST RENTAL MODELS</h1>
-        <div className="w-5/12 border-t border-gray-300 my-4 mx-auto" />
-        <div className="w-4/12 border-t border-gray-300 my-4 mx-auto" />
-      </div>
-
-      <div className='flex mt-32 mb-12'>
-        <div className="flex gap-10 p-4 w-max mx-80">
-          <button
-            className="px-6 py-2 text-black font-semibold rounded transition-colors duration-300 hover:text-white hover:bg-blue-700"
-            onClick={() => handleCategoryChange('All')}
-          >
-            <h4>SHOW ALL</h4>
-          </button>
-          <button
-            className="px-6 py-2 text-black font-semibold rounded transition-colors duration-300 hover:text-white hover:bg-blue-700"
-            onClick={() => handleCategoryChange('Van')}
-          >
-            <h6>VAN</h6>
-          </button>
-          <button
-            className="px-6 py-2 text-black font-semibold rounded transition-colors duration-300 hover:text-white hover:bg-blue-700"
-            onClick={() => handleCategoryChange('Sedan')}
-          >
-            <h6>SEDAN</h6>
-          </button>
-          <button
-            className="px-6 py-2 text-black font-semibold rounded transition-colors duration-300 hover:text-white hover:bg-blue-700"
-            onClick={() => handleCategoryChange('Hatchback')}
-          >
-            <h6>HATCHBACK</h6>
-          </button>
-          <button
-            className="px-6 py-2 text-black font-semibold rounded transition-colors duration-300 hover:text-white hover:bg-blue-700"
-            onClick={() => handleCategoryChange('SUV')}
-          >
-            <h6>SUV</h6>
-          </button>
-        </div>
-        <div className="flex gap-52 p-4 w-max mx-20">
-          <button className="px-6 py-2 bg-red-100 text-black font-semibold rounded transition-colors duration-300 hover:text-white hover:bg-blue-700">
-            ALL MODELS
-          </button>
-        </div>
-      </div>
-
-      <div>
-      <CategoryButtons onSelectCategory={setSelectedModel} />
-      <UsersCars selectedModel={selectedModel} />
-    </div>
-    </div>
-  );
-};
-
-export default Homepage;
-
-
-
-
-
-
-
 
 
 
 
 
 // src/LandingPage.jsx
-{/*import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -217,5 +106,5 @@ const Homepage = () => {
 
 
 
-export default Homepage;*/}
+export default Homepage;
 
