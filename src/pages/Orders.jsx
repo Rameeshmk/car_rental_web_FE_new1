a2f714d0180e9d9972bcd37956b1ec5f72a221d7
Rameeshmk <rameeshmk@gmail.com>
2024-09-03 11:45:12 +0300

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -106,7 +106,7 @@ const Orders = () => {
           const currentDate = new Date();
 
           // Filter out expired orders
-          const validOrders = data.filter(order => new Date(order.endDate) >= currentDate);
+          const validOrders = data.filter(order => new Date(orders.endDate) >= currentDate);
         
           setOrders(validOrders);
           
