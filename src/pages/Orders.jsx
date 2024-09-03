e0302b3eb2e39cdca8ec52bb588164497286baed
Rameeshmk <rameeshmk@gmail.com>
2024-09-03 10:43:21 +0300

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -49,7 +49,7 @@ const Orders = () => {
               <p className="text-lg text-gray-600">User ID: {order.userId}</p>
               <p className="text-lg text-gray-600">Order ID: {order._id}</p>
 
-              <h2 className="text-2xl font-semibold text-gray-700"> Car Details</h2>
+              <h2 className="text-2xl font-semibold text-gray-700"> Order Details</h2>
             
               <p className="text-lg text-gray-600">Car Name: {order.car.name}</p>
               <p className="text-lg text-gray-600">Model:{order.car.model}</p>
@@ -57,9 +57,9 @@ const Orders = () => {
               <p className="text-lg text-gray-600">Fuel Type:{order.car.fueltype} </p>
               
               <p className="text-lg text-gray-600">Rent per Day:{order.car.price} </p>
-              </div>
-              <div>
-              <h2 className="text-2xl font-semibold text-gray-700">Order Details</h2>
+              
+              
+              
               <p className="text-lg text-gray-600">Car Name: {order.car.name}</p>
               <p className="text-lg text-gray-600">Start Date: {new Date(order.startDate).toLocaleDateString()}</p>
               <p className="text-lg text-gray-600">End Date: {new Date(order.endDate).toLocaleDateString()}</p>
