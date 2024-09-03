5ee51721b4e2a38db303c940a7f21723d388f28d
Rameeshmk <rameeshmk@gmail.com>
2024-09-03 11:34:38 +0300

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -100,6 +100,7 @@ const Orders = () => {
         // Ensure response data is available and valid
         if (response.data && Array.isArray(response.data.data)) {
           const data = response.data.data;
+          console.log("data",data)
 
           // Get current date
           const currentDate = new Date();
