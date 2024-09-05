491a7dfdf1e8a09c8cfdf70600dcb07fbc2f15aa
Rameeshmk <rameeshmk@gmail.com>
2024-09-05 10:34:35 +0300

    Update dealersGarrage.jsx

--- src/components/dealer/dealersGarrage.jsx
+++ src/components/dealer/dealersGarrage.jsx
@@ -45,12 +45,12 @@ const DealersGarrage = () => {
       cars.map((car) => (
         <div key={car._id} className="flex flex-col lg:flex-row p-4 gap-8 mb-4 border border-gray-300 rounded-lg shadow-md">
           <div className="flex-1 flex items-center justify-center lg:pr-8">
-            <img src={car.image} alt={car.name} className="object-cover w-full lg:w-1/2 h-auto" />
+            <img src={car.image} alt={car.name} className="object-cover w-full lg:w-1/5 h-auto" />
           </div>
           <div className="flex-1">
             <h2 className="text-2xl font-bold">{car.name}</h2>
             <p className="text-black">Make: {car.make}</p>
-            <p className="text-black font-bold mt-2">₹{car.price}</p>
+            <p className="text-black font-bold mt-2">Rent Per Day ₹{car.price}</p>
             <p className="text-black mt-2">Model: {car.model}</p>
             <p className="text-black">Fuel Type: {car.fueltype}</p>
             <p className="text-black">Capacity: {car.capacity}</p>
