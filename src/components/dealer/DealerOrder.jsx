bf85ff06279196f3b3dd0935260ab5285e9a3ac8
Rameeshmk <rameeshmk@gmail.com>
2024-09-07 10:09:37 +0300

    Update DealerOrder.jsx

--- src/components/dealer/DealerOrder.jsx
+++ src/components/dealer/DealerOrder.jsx
@@ -76,7 +76,52 @@ const DealersOrder = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500 text-center text-6xl mt-52"> {error}</p>;
 
-  return (
+   // Get current date
+   const currentDate = new Date();
+
+
+   return (
+    <div>
+      <h1 className="text-4xl font-bold mb-6 text-center text-gray-800">Orders</h1>
+
+      {orders &&
+        orders.map(order => {
+          // Determine if the order is active or expired
+          const isActive = new Date(order.endDate) >= currentDate;
+          const status = isActive ? 'Active' : 'Expired';
+          const statusClass = isActive ? 'text-green-600' : 'text-red-600';
+
+          return (
+            <div key={order._id} className="border-b mb-4 pb-4 justify-center items-center">
+              <div className="p-6 md:p-12 lg:p-16 max-w-2xl mx-auto bg-white shadow-lg rounded-lg">
+                <p className="text-lg text-gray-600">User ID: {order.userId}</p>
+                <p className="text-lg text-gray-600">Order ID: {order._id}</p>
+
+                <h2 className="text-2xl font-semibold text-gray-700">Order Details</h2>
+
+                <p className="text-lg text-gray-600">Car Name: {order.car.name}</p>
+                <p className="text-lg text-gray-600">Model: {order.car.model}</p>
+                <p className="text-lg text-gray-600">Fuel Type: {order.car.fueltype}</p>
+                <p className="text-lg text-gray-600">Rent per Day: ₹{order.car.price}</p>
+                <p className="text-lg text-gray-600">Start Date: {new Date(order.startDate).toLocaleDateString()}</p>
+                <p className="text-lg text-gray-600">End Date: {new Date(order.endDate).toLocaleDateString()}</p>
+                <p className="text-lg font-semibold text-gray-800">Total Days: {order.totalDays}</p>
+                <p className="text-lg font-semibold text-gray-800">Total Amount: ₹{order.totalAmount}</p>
+                <p className="text-lg font-semibold text-gray-800">Pickup Location: {order.pickupLocation}</p>
+
+                <p className={`text-xl font-semibold ${statusClass}`}>Status: {status}</p>
+              </div>
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
+
+
+
+  {/*return (
     <div className="p-6 md:p-12 lg:p-16 max-w-4xl mx-auto bg-white shadow-lg rounded-lg">
       <h1 className="text-4xl font-bold mb-6 text-center text-gray-800">Your Orders</h1>
       
@@ -110,6 +155,6 @@ const DealersOrder = () => {
       
     </div>
   );
-};
+};*/}
 export default DealersOrder;
  
