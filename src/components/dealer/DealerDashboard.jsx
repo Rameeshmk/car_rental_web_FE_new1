03af1619240770195a1a118c60f0ada6169dfd3f
Rameeshmk <rameeshmk@gmail.com>
2024-09-09 09:11:28 +0300

    Update DealerDashboard.jsx

--- src/components/dealer/DealerDashboard.jsx
+++ src/components/dealer/DealerDashboard.jsx
@@ -7,9 +7,9 @@ const DealerDashboard = () => {
       className="h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('https://www.pixelstalk.net/wp-content/uploads/2016/08/1080p-Car-Wallpaper-Download-Free.jpg')" }} // Your car background image URL
     >
-      <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold text-white mb-8">
+      <div className="flex flex-col items-center justify-center h-full bg-black bg-opacity-50 p-4">
+       
+      <h1 className="text-3xl font-bold text-white mb-8 md:text-4xl lg:text-5xl">
             WELCOME TO DEALER DASHBOARD
           </h1>
           <div className="flex flex-col gap-4 sm:gap-6 md:flex-row md:flex-wrap md:gap-6 md:justify-center">
@@ -25,7 +25,7 @@ const DealerDashboard = () => {
             </button>
           </Link>
           <Link to="/dealer/dealerEditCar">
-            <button className="bg-red-600 text-white py-3 px-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <button className="w-full sm:w-48 bg-red-600 text-white py-3 px-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500">
               EDIT YOUR CAR
             </button>
           </Link>
@@ -34,7 +34,7 @@ const DealerDashboard = () => {
               DEALINGS
             </button>
           </Link>
-</div>
+
 
         </div>
       </div>
